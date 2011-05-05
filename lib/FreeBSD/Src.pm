a983e0461d921279e0fe5c117d144517d6c20f2f
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2011-05-05 08:52:27 +0000

    Import of VVELOX/FreeBSD-Src-2.0.0 from CPAN.
    gitpan-cpan-distribution: FreeBSD-Src
    gitpan-cpan-version:      2.0.0
    gitpan-cpan-path:         VVELOX/FreeBSD-Src-2.0.0.tar.gz
    gitpan-cpan-author:       VVELOX
    gitpan-cpan-maturity:     released

--- lib/FreeBSD/Src.pm
+++ lib/FreeBSD/Src.pm
@@ -9,32 +9,43 @@ FreeBSD::Src - A object oriented interface to building FreeBSD from source.
 
 =head1 VERSION
 
-Version 1.0.0
+Version 2.0.0
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '2.0.0';
 
 
 =head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
 
     use FreeBSD::Src;
 
 	#creates a new FreeBSD::Src object and build stuff in /tm[/obj and a kernel config
 	#named whatever.
     my $src=FreeBSD::Src->new({obj=>"/tmp/obj", kernel=>"whatever"});
-    
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
+
     #builds and installs the kernel and world
     $src->makeBuildKernel;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
     $src->makeInstallKernel;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
     $src->makeBuildWorld;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
     $src->makeInstallWorld;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
 
-=head1 FUNCTIONS
+=head1 METHODS
 
 =head2 new
 
@@ -52,63 +63,87 @@ The default is '/usr/src' if it is not defined.
 
 This contains the path to the directory that will contain objects created by the compilation.
 
-The default is '/usr/obj' if it is not defined.
+If not defined, the enviromental variable "MAKEOBJDIR" will be left
+as it is.
 
 =head3 kernel
 
 This is the kernel config that will be build.
 
-The default is 'GENERIC' unless it is defined.
+If this is defined, it is left up to make to figure out which should be used. For
+more information, please see the man page make.conf(5).
 
 =head3 makeconf
 
 This is the make.conf file to use for the build.
 
+    my $src=FreeBSD::Src->new({obj=>"/tmp/obj", kernel=>"whatever"});
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
+
 =cut
 
 sub new{
-	my %args= %{$_[1]};
+	my %args;
+	if (defined( $_[1] )){
+		%args=%{$_[1]};
+	}
 
 	#create the object that will be passed around
 	my $self = {conf=>{}, error=>undef, returnedInt=>undef,
 				returned=>undef, permanentError=>undef};
 	bless $self;
 
+	#this will be appended to the end of make
+	$self->{conf}{makeappend}="";
+
+	#gets what to use as the src directory
 	if(!defined($args{src})){
 		$self->{conf}{src}="/usr/src";
 	}else{
 		$self->{conf}{src}=$args{src};
-	};
+	}
 
-	#figures out what to use for obj dir prefix
+	#gets what to use for the destination directory
+	if (defined($args{destdir})){
+		$self->{conf}{destdir}=$args{destdir};
+		$self->{conf}{makeappend}=$self->{conf}{makeappend}.
+		" DESTDIR='".$self->{conf}{destdir}."'";
+	}
+
+	#figures out what to use for obj dir
 	if(!defined($args{obj})){
-		$self->{conf}{obj}="/usr/obj";
+		$self->{conf}{obj}=undef;
 	}else{
 		$self->{conf}{obj}=$args{obj};
-	};
-	#sets the obj dir prefix
-	$ENV{MAKEOBJDIRPREFIX}=$self->{conf}{obj};
+	}
+	#sets the obj dir
+	if (defined($self->{conf}{obj})) {
+		$ENV{MAKEOBJDIR}=$self->{conf}{obj};
+	}
 
 	if(!defined($args{kernel})){
-		$self->{conf}{kernel}="GERNERIC";
+		$self->{conf}{kernel}=undef;
 	}else{
 		$self->{conf}{kernel}=$args{kernel};
-	};
+	}
 
 	#sets the make.conf
 	if(!defined($args{makeconf})){
 		$self->{conf}{makeconf}="/etc/make.conf";
 	}else{
 		$self->{conf}{makeconf}=$args{makeconf};
-	};
+	}
 
 	if(!-e $self->{conf}{src}){
 		warn('FreeBSD::Src error:0: The directory source directory, '.$self->{conf}{src}.', does not exist.');
-		$self->{permanentError}="0";
-	};
+		$self->{permanentError}="5";
+		$self->{error}="5";
+	}
 
 	return $self;
-};
+}
 
 =head2 makeBuildWorld
 
@@ -116,9 +151,12 @@ Builds the world.
 
 The return is a perl boolean value.
 
-The returned integer from make can be reached in $obj->{returnedInt}.
+The returned integer from make can be found using the exitint method.
 
-The output from make can be found in $obj->{returned}.
+    $src->makeBuildWorld;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
 
 =cut
 
@@ -128,19 +166,20 @@ sub makeBuildWorld{
 	if(defined($self->{permanentError})){
 		$self->{error}=$self->{permanentError};
 		return undef;
-	};
+	}
 
 	$self->errorBlank();
 
 	chdir($self->{conf}{src});
 
-	$self->{returned}=`make buildworld __MAKE_CONF=$self->{conf}{makeconf}`;
+	$self->{returned}=`make buildworld __MAKE_CONF=$self->{conf}{makeconf} $self->{conf}{makeappend}`;
 	$self->{returnedInt}=$?;
 	if (!$self->{returnedInt} == 0){
 		$self->{error}=1;
 		return undef;
-	};
-};
+	}
+	return 1;
+}
 
 =head2 makeInstallWorld
 
@@ -148,9 +187,12 @@ Installs the world.
 
 The return is a perl boolean value.
 
-The returned integer from make can be reached in $obj->{returnedInt}.
+The returned integer from make can be found using the exitint method.
 
-The output from make can be found in $obj->{returned}.
+    $src->makeInstallWorld;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
 
 =cut
 
@@ -160,19 +202,20 @@ sub makeInstallWorld{
 	if(defined($self->{permanentError})){
 		$self->{error}=$self->{permanentError};
 		return undef;
-	};
+	}
 
 	$self->errorBlank();
 
 	chdir($self->{conf}{src});
 
-	$self->{returned}=`make installworld __MAKE_CONF=$self->{conf}{makeconf}`;
+	$self->{returned}=`make installworld __MAKE_CONF=$self->{conf}{makeconf}  $self->{conf}{makeappend}`;
 	$self->{returnedInt}=$?;
 	if (!$self->{returnedInt} == 0){
 		$self->{error}=2;
 		return undef;
-	};
-};
+	}
+	return 1;
+}
 
 =head2 makeBuildKernel
 
@@ -180,9 +223,12 @@ Builds the kernel
 
 The return is a perl boolean value.
 
-The returned integer from make can be reached in $obj->{returnedInt}.
+The returned integer from make can be found using the exitinit method.
 
-The output from make can be found in $obj->{returned}.
+    $src->makeBuildKernel;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
 
 =cut
 
@@ -192,19 +238,26 @@ sub makeBuildKernel{
 	if(defined($self->{permanentError})){
 		$self->{error}=$self->{permanentError};
 		return undef;
-	};
+	}
 
 	$self->errorBlank();
 
 	chdir($self->{conf}{src});
 
-	$self->{returned}=`make buildkernel __MAKE_CONF=$self->{conf}{makeconf} KERNCONF=$self->{conf}{kernel}`;
+	my $makecommand='make buildkernel __MAKE_CONF='.$self->{conf}{makeconf};
+	if (defined($self->{conf}{kernel})) {
+		$makecommand=$makecommand.' KERNCONF='.$self->{conf}{kernel};
+	}
+	$makecommand=$makecommand.' '.$self->{conf}{makeappend};
+
+	$self->{returned}=`$makecommand`;
 	$self->{returnedInt}=$?;
 	if (!$self->{returnedInt} == 0){
 		$self->{error}=3;
 		return undef;
-	};
-};
+	}
+	return 1;
+}
 
 =head2 makeInstallKernel
 
@@ -212,9 +265,12 @@ Install the kernel.
 
 The return is a perl boolean value.
 
-The returned integer from make can be reached in $obj->{returnedInt}.
+The returned integer from make can be reached in exitint method.
 
-The output from make can be found in $obj->{returned}.
+    $src->makeInstallKernel;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
 
 =cut
 
@@ -224,22 +280,102 @@ sub makeInstallKernel{
 	if(defined($self->{permanentError})){
 		$self->{error}=$self->{permanentError};
 		return undef;
-	};
+	}
 
 	$self->errorBlank();
 
 	chdir($self->{conf}{src});
 
-	$self->{returned}=`make buildkernel __MAKE_CONF=$self->{conf}{makeconf} KERNCONF=$self->{conf}{kernel}`;
+	$self->{returned}=`make installkernel`;
 	$self->{returnedInt}=$?;
 	if (!$self->{returnedInt} == 0){
 		$self->{error}=4;
 		return undef;
-	};
-};
+	}
+	return 1;
+}
+
+=head2 makeClean
+
+This cleans the build enviroment.
+
+The return is a perl boolean value.
+
+The returned integer from make can be found using the exitinit method.
+
+    $src->makeClean;
+    if($src->error){
+        warn('Error: '.$src->error);
+    }
+
+=cut
+
+sub makeClean{
+	my ($self)= @_;
+
+	if(defined($self->{permanentError})){
+		$self->{error}=$self->{permanentError};
+		return undef;
+	}
+
+	$self->errorBlank();
+
+	chdir($self->{conf}{src});
+
+	$self->{returned}=`make clean`;
+	$self->{returnedInt}=$?;
+	if (!$self->{returnedInt} == 0){
+		$self->{error}=3;
+		return undef;
+	}
+	return 1;
+}
 
 
-=head2 errorBlank 
+=head2 output
+
+This gets the stdout from make from the previously called method.
+
+    my $output=$src->output;
+
+=cut
+
+sub output{
+	my $self=$_[0];
+
+	return $self->{returned};
+}
+
+=head1 ERROR RELATED METHODS
+
+=head2 error
+
+This returns the current error code, if any.
+
+    my $error=$src->error;
+    if($error){
+        warn('Error Code: '.$error);
+    }
+
+=cut
+
+sub error{
+	return $_[0]->{error};
+}
+
+=head2 exitint
+
+This is exit int from the make of the last called method.
+
+
+
+=cut
+
+sub exitint{
+	return $_[0]->{returnedInt};
+}
+
+=head2 errorBlank
 
 This is a internal function and should not be called.
 
@@ -250,10 +386,9 @@ sub errorBlank{
         my $self=$_[0];
 
         $self->{error}=undef;
-        $self->{returnedInt}=undef;
 
         return 1;
-};
+}
 
 =head1 ERROR CODES
 
@@ -277,9 +412,13 @@ The source directory does not exist.
 
 'make installkernel KERNCONF=<kernel>' failed.
 
+=head2 5
+
+The source directory does not exist.
+
 =head1 AUTHOR
 
-Zane C. Bowers, C<< <vvelox at vvelox.net> >>
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
 
 =head1 BUGS
 
@@ -322,7 +461,7 @@ L<http://search.cpan.org/dist/FreeBSD-Src>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Zane C. Bowers, all rights reserved.
+Copyright 2011 Zane C. Bowers, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
